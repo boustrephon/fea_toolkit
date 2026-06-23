a3c84f30e37514ee288d63eae72fe596738a92d0
boustrephon <andrew.mole@gmail.com>
2026-06-23 20:15:55 +0800

    Create __init__.py
    Added placeholder for Rhino folder

---
+++ src/fea_toolkit/rhino/__init__.py
@@ -0,0 +1,5 @@
+"""Rhino visualisation module (placeholder).
+
+This module is intended for exporting SAPModelData to Rhino for 3D visualisation.
+The importer code from sap2000_import_v8.py should be refactored here.
+"""
